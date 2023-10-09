642b22ee2b98f46bd7620186c4b5606b19be96ea
Gabriel Campos <66139106+panppa@users.noreply.github.com>
2023-10-09 17:40:33 -0300

    Update TestLogin.java
    arrumando as aspas

--- TestLogin.java
+++ TestLogin.java
@@ -11,8 +11,8 @@ public class TestLogin {
 		  driver.get("https://panppa.github.io/Eplayers/");
 		  driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
       		  driver.findElement(By.id("inscrevaseButton”)).click(); 
-		  driver.findElement(By.id ("email ")).sendKeys("gabrielfcampos2017@gmail.com");
-      		  driver.findElement(By.id ("senha ")).sendKeys("123Quatro56!”);
+		  driver.findElement(By.id ("email")).sendKeys("gabrielfcampos2017@gmail.com");
+      		  driver.findElement(By.id ("senha")).sendKeys("123Quatro56!");
 		  driver.findElement(By.id("loginButton")).click(); 
   }
 }
