a95c08da596492c7b65cd9dac1cc5eb908be33d6
Gabriel Campos <66139106+panppa@users.noreply.github.com>
2023-10-09 17:39:29 -0300

    Update TestLogin.java

--- TestLogin.java
+++ TestLogin.java
@@ -10,9 +10,9 @@ public class TestLogin {
 	 public void testeNavegacao() { 
 		  driver.get("https://panppa.github.io/Eplayers/");
 		  driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-      driver.findElement(By.id("inscrevaseButton”)).click(); 
+      		  driver.findElement(By.id("inscrevaseButton”)).click(); 
 		  driver.findElement(By.id ("email ")).sendKeys("gabrielfcampos2017@gmail.com");
-      driver.findElement(By.id ("senha ")).sendKeys("123Quatro56!”)
+      		  driver.findElement(By.id ("senha ")).sendKeys("123Quatro56!”);
 		  driver.findElement(By.id("loginButton")).click(); 
   }
 }
