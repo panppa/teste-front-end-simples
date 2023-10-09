1dc4d7866293ead6b070231f0d2dbcb4211512a2
Gabriel Campos <66139106+panppa@users.noreply.github.com>
2023-10-09 17:38:06 -0300

    Create TestLogin.java

---
+++ TestLogin.java
@@ -0,0 +1,18 @@
+public class TesteBuscaCursos {
+	private WebDriver driver;
+	@Before
+	public void abrirNavegador( ) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	 @Test 
+	 public void testeNavegacao() { 
+		  driver.get("https://panppa.github.io/Eplayers/");
+		  driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+      driver.findElement(By.id("inscrevaseButton”)).click(); 
+		  driver.findElement(By.id ("email ")).sendKeys("gabrielfcampos2017@gmail.com");
+      driver.findElement(By.id ("senha ")).sendKeys("123Quatro56!”)
+		  driver.findElement(By.id("loginButton")).click(); 
+  }
+}
